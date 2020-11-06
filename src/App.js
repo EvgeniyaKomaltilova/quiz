77eed08ef95d27e2a0a75ffd662d15e62e1b96b6
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-11-06 17:19:43 +0700

    added Layout and Quiz components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './hoc/Layout/Layout'
+import Quiz from './hoc/containers/Quiz/Quiz'
+
+
+function App() {
+  return (
+    <Layout>
+      <Quiz />
+    </Layout>
+  );
+}
+
+export default App;

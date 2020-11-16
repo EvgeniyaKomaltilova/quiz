6eb9208e23c968fcc9c92eeb2bc87b0da9b71f1d
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-11-16 13:58:31 +0700

    answer onclick handler added

--- src/hoc/components/ActiveQuiz/ActiveQuiz.js
+++ src/hoc/components/ActiveQuiz/ActiveQuiz.js
@@ -6,14 +6,14 @@ const ActiveQuiz = props => (
 <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
         <span>
-            <strong>2.</strong>&nbsp;
-            How are you?
+            <strong>1.</strong>&nbsp;
+            {props.question}
         </span>
 
         <small>4 from 12</small>
     </p>
 
-    <AnswersList answers={props.answers} />
+    <AnswersList answers={props.answers} onAnswerClick={props.onAnswerClick} />
 
 </div>
 )
--- src/hoc/containers/Quiz/Quiz.js
+++ src/hoc/containers/Quiz/Quiz.js
@@ -6,14 +6,20 @@ class Quiz extends Component {
     state = {
         quiz: [
             {
+                question: 'What color is the sky?',
+                rightAnswerId: 2,
                 answers: [
-                    {text: 'Answer #1'},
-                    {text: 'Answer #2'},
-                    {text: 'Answer #3'},
-                    {text: 'Answer #4'}
+                    {text: 'Blue', id: 1},
+                    {text: 'Red', id: 2},
+                    {text: 'Green', id: 3},
+                    {text: 'Yellow', id: 4}
                 ]
             }
         ]
+    }
+
+    onAnswerClickHandler = (answerId) => {
+        console.log(answerId)
     }
 
     render() {
@@ -21,7 +27,11 @@ class Quiz extends Component {
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Please answer all questions</h1>
-                    <ActiveQuiz answers={this.state.quiz[0].answers} />
+                    <ActiveQuiz 
+                    question={this.state.quiz[0].question} 
+                    answers={this.state.quiz[0].answers} 
+                    onAnswerClick={this.onAnswerClickHandler}
+                    />
                 </div>
             </div>
         )

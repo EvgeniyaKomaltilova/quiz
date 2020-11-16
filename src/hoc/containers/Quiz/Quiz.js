ab6151f52c919c23c5a36510a8243e21c9fb6bc0
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-11-16 12:16:32 +0700

    answers list added

--- src/hoc/containers/Quiz/Quiz.js
+++ src/hoc/containers/Quiz/Quiz.js
@@ -4,15 +4,24 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {
+                answers: [
+                    {text: 'Answer #1'},
+                    {text: 'Answer #2'},
+                    {text: 'Answer #3'},
+                    {text: 'Answer #4'}
+                ]
+            }
+        ]
     }
 
     render() {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Quiz</h1>
-                    <ActiveQuiz />
+                    <h1>Please answer all questions</h1>
+                    <ActiveQuiz answers={this.state.quiz[0].answers} />
                 </div>
             </div>
         )
